d885accc9a27720c5988692c5dd330ce7bda799a
Karthick Ragavendran <karthickthankyou@gmail.com>
2021-06-03 21:24:09 +0530

    added default font size

--- src/components/atoms/Text/Text.tsx
+++ src/components/atoms/Text/Text.tsx
@@ -1,8 +1,8 @@
 export interface ITextProps {
-  size: 'xs' | 'sm' | 'base' | 'lg' | 'xl'
+  size?: 'xs' | 'sm' | 'base' | 'lg' | 'xl'
 }
 
-const Text = ({ size }: ITextProps) => {
+const Text = ({ size = 'base' }: ITextProps) => {
   return (
     <div className={`text-${size}`}>🛬 Hello, This is Text component! 🛫</div>
   )
